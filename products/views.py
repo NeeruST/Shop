01454f2aaa2487da2f511d5d6c850dd537e4eb71
neeraj singh <89096690+NeeruST@users.noreply.github.com>
2021-09-08 13:21:26 +0530

    Initial commit

---
+++ products/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Product
+
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
+
+
+def index1(request):
+    return HttpResponse('hey there, new products will added here.')
